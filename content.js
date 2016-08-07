2e08f311e2b6571778c3ae700dddefc0ee442022
mwong678 <matthew.wong@wisc.edu>
2016-08-07 01:05:56 -0500

    test
    test

--- content.js
+++ content.js
@@ -1,82 +1,88 @@
 var txt;
 var listOfTitles = [];
+var listOfTitlesHash = {};
+$(document).ready(function() {
+  init();
+});
 
-window.onload = function(){
-	var name = getUserName();
-	if (!name){
-		return;
-	}
-	
+function init() {
+  var name = getUserName();
+  if (!name) {
+    console.log("User is not logged in!");
+    return;
+  }
+
   var xhttp = new XMLHttpRequest();
-  var url = 'http://myanimelist.net/animelist/'+name;
+  var url = 'http://myanimelist.net/animelist/' + name;
   xhttp.onload = function() {
-	if (parseXHTTP(xhttp.responseText)){
-		highLightTitles(listOfTitles);
-	}else{
-		console.log("Failed");
-	}
+    if (parseXHTTP(xhttp.responseText)) {
+      highLightTitles(listOfTitles);
+    } else {
+      console.log("Failed to parse user anime list");
+    }
   };
   xhttp.onerror = function() {
-    console.log("Error");
+    console.log("XHTTP ERROR");
   };
   xhttp.open("GET", url, true);
   xhttp.send(null);
+  return true;
+
 }
 
-function getUserName(){
-	userName = null;
-	var userNameLink = document.getElementsByClassName("header-profile-link")[0];
-	if (!userNameLink){
-		return null;
-	}else{
-		userName = userNameLink.text;
-	}
-	return userName;
+function getUserName() {
+  userName = null;
+  var userNameLink = document.getElementsByClassName("header-profile-link")[0];
+  if (!userNameLink) {
+    return null;
+  } else {
+    userName = userNameLink.text;
+  }
+  return userName;
 }
 
-function highLightTitles(list){
-	var tables = document.getElementsByTagName("Table");
-	var row = tables[1].getElementsByTagName("TR");
-	var titleTable = row[0].getElementsByTagName("Table");
+function highLightTitles(list) {
+  var tables = document.getElementsByTagName("Table");
+  var row = tables[1].getElementsByTagName("TR");
+  var titleTable = row[0].getElementsByTagName("Table");
+  if (titleTable.length > 0) {
     for (var i = 0, row; row = titleTable[0].rows[i]; i++) {
-		for (var j = 0, col; col = row.cells[j]; j++) {
-			if (j == 1){
-				aLink = col.getElementsByTagName("A");
-				if (searchTitle(aLink[0].text)){
-					row.setAttribute("style", "background-color: yellow;");
-				}
-			}
-		}  
-	}
-	
+      for (var j = 0, col; col = row.cells[j]; j++) {
+        if (j == 1) {
+          aLink = col.getElementsByTagName("A");
+          if (searchTitle(aLink[0].text)) {
+            row.setAttribute("style", "background-color: yellow;");
+          }
+        }
+      }
+    }
+  } else {
+		//if you pressed back after choosing a character on the page the tables
+		//wouldn't load in time, init will try again.
+    init();
+  }
+
 }
 
-function parseXHTTP(text){
-	var getInfo = document.createElement('TD');
-	getInfo.innerHTML = text;
-	var rows = getInfo.getElementsByClassName('list-table')[0];
-	txt = rows.getAttribute('data-items');
-	var json = JSON.parse(txt);
-	if (!json){
-		return false;
-	}
-	for (var i = 0; i < json.length; i++){
-		listOfTitles.push(json[i].anime_title);
-	}
-	return true;
+function parseXHTTP(text) {
+  var getInfo = document.createElement('TD');
+  getInfo.innerHTML = text;
+  var rows = getInfo.getElementsByClassName('list-table')[0];
+  txt = rows.getAttribute('data-items');
+  var json = JSON.parse(txt);
+  if (!json) {
+    return false;
+  }
+  for (var i = 0; i < json.length; i++) {
+    listOfTitlesHash[json[i].anime_title] = 1;
+  }
+  return true;
 }
 
-function searchTitle(title){
-	if (!listOfTitles){
-		return false;
-	}
-	for (var i = 0; i < listOfTitles.length;i++){
-		
-		if (title == listOfTitles[i]){
-			return true;
-		}
-	}
-	
-	return false;
+function searchTitle(title) {
+  if (listOfTitlesHash[title]) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
