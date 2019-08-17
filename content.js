e77f02664ffb01e4a6ac25c513d62f86806e0672
mwong678 <matthew.wong@wisc.edu>
2019-08-17 11:19:16 -0700

    '2.0.0'

--- content.js
+++ content.js
@@ -1,135 +1,63 @@
-var txt;
-var listOfTitlesHash = {};
-var listOfStatusHeadersHash = {}; // title to number of entries
-var order = ["Currently Watching", "Completed", "On Hold", "Dropped", "Plan to Watch"];
-//$(document).ready(function() {init();});
-document.onload = init();
+let titleToWatchStatus = {};
 
-function init() {
-  var name = getUserName();
-  
-  console.log(name);
-  if (!name) {
-    console.log("User is not logged in!");
+async function init(){
+  const name = getUserName();
+  let offset = 0;
+
+  if (!name) return;
+
+  let url = `https://myanimelist.net/animelist/${name}/load.json?offset=${offset}status=7`,
+      profileResponse = await fetch(url),
+      profileResponseJSON = await profileResponse.json();
+
+  if (!profileResponseJSON){
+    console.log('Failed to get user\'s anime list');
     return;
   }
 
-  var xhttp = new XMLHttpRequest();
-  var url = 'https://myanimelist.net/animelist/' + name;
-  //var url = 'https://myanimelist.net/animelist/Memor';
-  xhttp.onload = function() {
-    if (parseXHTTP(xhttp.responseText)) {
-      highLightTitles();
-    } else {
-      console.log("Failed to parse user anime list");
-    }
-  };
-  xhttp.onerror = function() {
-    console.log("XHTTP ERROR");
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send(null);
-  return true;
+  while (profileResponseJSON.length > 0){
+    parseList(profileResponseJSON);
+    offset += profileResponseJSON.length;
+    url = `https://myanimelist.net/animelist/${name}/load.json?offset=${offset}status=7`;
+    profileResponse = await fetch(url),
+    profileResponseJSON = await profileResponse.json();
+  }
 
+  highLightTitles();
 }
 
 function getUserName() {
-  userName = null;
-  var userNameLink = document.getElementsByClassName("header-profile-link")[0];
-  if (!userNameLink) {
-    return null;
-  } else {
-    userName = userNameLink.text;
-  }
-  return userName;
+  let userName = null;
+  let userNameLink = document.getElementsByClassName('header-profile-link')[0];
+  if (!userNameLink) return null;
+  else return userNameLink.text;
 }
 
 function highLightTitles(list) {
-  var tables = document.getElementsByTagName("Table");
-  var row = tables[1].getElementsByTagName("TR");
-  if (row.length > 0) {
-    for (var i =0; i < row.length; i++){
-      var td = row[i].getElementsByTagName("TD");
-      for (var j = 0; j < td.length; j++) {
-        if (j == 1) {
-          aLink = td[j].getElementsByTagName("A");
-          if (searchTitle(aLink[0].text)) {
-            if (listOfTitlesHash[aLink[0].text] != 6){
-              //will not highlight planned to watch titles
-              row[i].setAttribute("style", "background-color: yellow;");
-            }
-          }
-        }
+  const tables = document.getElementsByTagName('Table'),
+        rows = tables[1].getElementsByTagName('TR');
+
+  if (rows.length > 0) {
+    for (let i = 0; i < rows.length; i++){
+      let td = rows[i].getElementsByTagName('TD'),
+          aLink = td[1].getElementsByTagName('A'),
+          title = aLink[0].text;
+
+      if (titleToWatchStatus[title] && titleToWatchStatus[title] != 6) {
+          rows[i].setAttribute('style', 'background-color: yellow;');
       }
     }
   } else {
-		//if you pressed back after choosing a character on the page the tables
-		//wouldn't load in time, init will try again.
-    init();
-  }
-
-}
-
-function parseXHTTP(text) {
-  var getInfo = document.createElement('TD');
-  getInfo.innerHTML = text;
-  var rows = getInfo.getElementsByClassName('list-table')[0];
-
-  if (rows){
-    //classic theme
-    txt = rows.getAttribute('data-items');
-    var json = JSON.parse(txt);
-    if (!json) {
-      return false;
-    }
-    for (var i = 0; i < json.length; i++) {
-      listOfTitlesHash[json[i].anime_title] = json[i].status;
-    }
-    return true;
-
-  }else{
-    rows = getInfo.getElementsByClassName('status_not_selected');
-    for (var i = 0; i < rows.length;i++){
-      listOfStatusHeadersHash[rows[i].getElementsByTagName('A')[0].innerText.trim()] = rows[i].getElementsByTagName('A')[0].getAttribute('title').split(" ")[0];
-      //console.log(rows[i].getElementsByTagName('A')[0].innerText.trim()+ "     " + rows[i].getElementsByTagName('A')[0].getAttribute('title').split(" ")[0]);
-    }
-
-    count = 0;
-    statusNum = 1;
-    rows = getInfo.getElementsByClassName('animetitle');
-    for (var i = 0; i < order.length; i++){
-      status = order[i];
-      switch (status){
-        case "Currently Watching":
-          statusNum = 1;
-          break;
-        case "Completed":
-          statusNum = 2;
-          break;
-      case "On Hold":
-        statusNum = 3;
-        break;
-      case "Dropped":
-        statusNum = 4;
-        break;
-      case "Plan to Watch":
-        statusNum = 6;
-        break;
-      }
-      for (var j = 0; j < listOfStatusHeadersHash[status]; j++){
-        listOfTitlesHash[rows[count].innerText.trim()] = statusNum;
-        //console.log(rows[count].innerText.trim() + "  "+ statusNum);
-        count++;
-      }
-    }
-    return true;
+    console.log('Failed to highlight titles');
   }
 }
 
-function searchTitle(title) {
-  if (listOfTitlesHash[title]) {
-    return true;
-  } else {
-    return false;
-  }
+function parseList(responseJSON){
+  if (!responseJSON) return false;
+  responseJSON.forEach(function(show){
+    titleToWatchStatus[show.anime_title] = show.status;
+  });
+  return true;
 }
+
+document.onload = init();
